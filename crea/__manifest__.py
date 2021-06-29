b38a7e98ad6ae54e84ba1fde8c1d900d06dde26f
Sucely Batz <sucelybch19@gmail.com>
2021-06-29 11:41:56 -0600

    crea

---
+++ crea/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "CREA",
+
+    'summary': """ Módulo crea """,
+
+    'description': """
+        Módulo crea
+    """,
+
+    'author': "Jefferson Silva",
+    'website': "https://www.sispavgt.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['account'],
+
+    'data': [
+        # 'data/rrhh_data.xml',
+        # 'data/paperformat_libro_salarios.xml',
+        # 'data/hr_payroll_expense_data.xml',
+        # 'views/hr_views.xml',
+        # 'views/hr_contract_views.xml',
+        # 'views/hr_payroll_views.xml',
+        # 'views/planilla_views.xml',
+        # 'views/res_company_views.xml',
+        'views/factura.xml',
+        'views/report.xml',
+        # 'wizard/planilla_pdf.xml',
+        # 'wizard/planilla.xml',
+        # 'views/libro_salarios.xml',
+        # 'views/hr_work_entry_views.xml',
+        # 'wizard/rrhh_libro_salarios_view.xml',
+        # 'wizard/rrhh_informe_empleador_view.xml',
+        # 'wizard/igss.xml',
+        # 'wizard/cerrar_nominas.xml',
+        # 'security/ir.model.access.csv',
+    ],
+}
